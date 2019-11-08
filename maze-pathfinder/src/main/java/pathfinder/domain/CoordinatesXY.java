cfea13bce3fd7fec20963fc3736158554efbe912
johyry <johannes.hyry@hotmail.com>
2019-11-08 09:33:19 +0200

    leveyshaun implementointi, apuluokat coordinatesxy ja mazeanalyzer, bitmapreader, yksikkötestejä ja traviksen käyttöönottoa

---
+++ maze-pathfinder/src/main/java/pathfinder/domain/CoordinatesXY.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pathfinder.domain;
+
+/**
+ *
+ * @author johyry
+ */
+public class CoordinatesXY {
+
+    private int x;
+    private int y;
+
+    public CoordinatesXY(int y, int x) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "y: " + y + ", x: " + x;
+    }
+
+}
